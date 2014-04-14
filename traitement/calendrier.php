36290769875ed98404fdd9fe8f91e7ac24c5be02
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-04-14 17:23:56 +0200

    test calendar

--- traitement/calendrier.php
+++ traitement/calendrier.php
@@ -1,1 +1,1 @@
-<iframe src="https://www.google.com/calendar/embed?src=mik5tf3ics83730b53q4099c4ebu99a7%40import.calendar.google.com&ctz=Europe/Paris" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://www.google.com/calendar/embed?src=81oih6btp059542v3p1vap1m6k%40group.calendar.google.com&ctz=Europe/Paris" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
