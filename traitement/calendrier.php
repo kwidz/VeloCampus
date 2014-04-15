9cbefa33a3ff473f0f74207322f06442affbb7a0
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-15 16:33:07 +0200

    centrercal

--- traitement/calendrier.php
+++ traitement/calendrier.php
@@ -8,7 +8,7 @@
 
 
 <div class="row">
-	<div class="span12">
+	<div class="col-md-12">
 		<center>
 		<iframe src="https://www.google.com/calendar/embed?src=81oih6btp059542v3p1vap1m6k%40group.calendar.google.com&ctz=Europe/Paris" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 	</center>
