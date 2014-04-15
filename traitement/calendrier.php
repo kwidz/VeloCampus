d199361c7ee4058eae70203fbd8d5ef66606cadb
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-15 20:46:20 +0200

    minimodif

--- traitement/calendrier.php
+++ traitement/calendrier.php
@@ -7,9 +7,12 @@
 
 
 
+
+
 <div class="row">
 	<div class="col-md-12">
 		<center>
+			
 		<iframe src="https://www.google.com/calendar/embed?src=81oih6btp059542v3p1vap1m6k%40group.calendar.google.com&ctz=Europe/Paris" style="border: 0" width="1000" height="600" frameborder="0" scrolling="no"></iframe>
 	</center>
    </div>	
--- traitement/login.php
+++ traitement/login.php
@@ -4,37 +4,3 @@
 	include("../footer.html");
 ?>
 
-<div class="row"> <!-- permet d'ouvrir une nouvelle ligne puis une nouvelle cellule -->
-	<div class="col-md-12"><!-- utiliser se lien ensuite pour faire les formulaires : http://getbootstrap.com/css/#forms  -->
-		<form class="form-horizontal" role="form">
-  			<div class="form-group">
-    			<label for="inputEmail3" class="col-sm-5 control-label">Email</label>
-    			<div class="col-sm-3">
-      				<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-    			</div>
-  			</div>
-  			<div class="form-group">
-    			<label for="inputPassword3" class="col-sm-5 control-label">Password</label>
-    			<div class="col-sm-3">
-      				<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-    			</div>
- 			 </div>
-  			<div class="form-group">
-    			<div class="col-sm-offset-5 col-sm-10">
-      				<div class="checkbox">
-        				<label><input type="checkbox"> Remember me</label>
-      				</div>
-    			</div>
-			</div>
-  			<div class="form-group">
-    			<div class="col-sm-offset-5 col-sm-10">
-      				<button type="submit" class="btn btn-default">Sign in</button>
-   				</div>
-  			</div>
-</form>
-
-
-
-
-	</div>
-</div>
