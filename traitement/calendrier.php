957f7286f7a8d93df3d52316ce7bf6a7b48483ef
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-16 16:14:59 +0200

    color

--- traitement/calendrier.php
+++ traitement/calendrier.php
@@ -8,15 +8,20 @@
 
 
 
-<br/><br/>
-<div class="row">
-	<div class="col-md-12">
+<div class="row" style="background-color:Gainsboro;border-radius:10px;border:3px solid #222222" >
+  <div class="col-md-12">
+  	<br/><br/>
 		<center>
 			
 		<iframe src="https://www.google.com/calendar/embed?src=81oih6btp059542v3p1vap1m6k%40group.calendar.google.com&ctz=Europe/Paris" style="border: 0" width="1000" height="600" frameborder="0" scrolling="no"></iframe>
 	</center>
-   </div>	
+
+<br/><br/>
+  </div>
 </div>
+
+       
+
 
 
 
--- traitement/photos.php
+++ traitement/photos.php
@@ -1,11 +1,17 @@
 <?php
 	include("../header.html");
 	include("../menu.html");
+
+?>
+<div class="row" style="background-color:Gainsboro;border-radius:10px;border:3px solid #222222" >
+  <div class="col-md-12">
+        EXEMPLE : <br/>
+        il faut ecrire dedans
+
+  </div>
+</div>
+
+
+<?php
 	include("../footer.html");
 ?>
-
-<html>
-
-<!-- <div class="wrapper">
-	</div> -->
-</html>
