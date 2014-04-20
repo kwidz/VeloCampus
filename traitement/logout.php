e00a1e44ce397ece74e732c76e0b3630bd5a64fc
Kiwhacks <pierre.limballe@hotmail.fr>
2014-04-20 20:23:21 +0200

    Correction du bug cookie sur chrome

--- traitement/logout.php
+++ traitement/logout.php
@@ -2,7 +2,7 @@
 	session_start();
 	$_SESSION = array(); 
 	session_destroy();
-	setcookie ("Session", "", time() - 3600, "/", "localhost");
+	setcookie ("Session", "", time() - 3600, "/", null);
 	// setcookie("Session", NULL, -1); 
 	header("Location: ".$_SERVER['HTTP_REFERER']);
 ?>
