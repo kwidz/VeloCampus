bc579571d66473b8d59662aed489f860dd47c1f3
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-21 18:37:48 +0200

    postitphp

--- Admin/traitement/postit.php
+++ Admin/traitement/postit.php
@@ -1,1 +1,12 @@
+<?php 
+require_once("../co.php");
+	if (isset($_POST['nom']) && isset($_POST['mess'])){
+		$nom = $_POST['nom'];
+		$mess=$_POST['mess'];
 
+		$sql = "INSERT INTO Postit VALUES (null,'".$nom."','".$mess."')";
+		$res = $mysqli->query($sql);
+	}
+	else {echo "erreur d'envoi";}
+header("Location: ".$_SERVER['HTTP_REFERER']);
+?>
