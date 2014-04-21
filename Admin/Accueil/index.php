43a8864ac7f921a2682e2a89f3ea79e4be2c5039
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-21 12:02:44 +0200

    admin

--- Admin/Accueil/index.php
+++ Admin/Accueil/index.php
@@ -14,7 +14,38 @@
     }
 ?>
 
+<div class="row">
+    <div class="col-md-3" >
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-success">Importer Calendrier google</a>
+          <a href="#" class="list-group-item list-group-item-info">Modifier texte de l'acceuil</a>
+          <a href="#" class="list-group-item list-group-item-warning">Modifier photos du slide</a>
+          <a href="#" class="list-group-item list-group-item-danger">Ajouter photos d'évenements</a>
+          <a href="#" class="list-group-item list-group-item-danger">Supprimmer tous les adhérents</a>
+          <a href="#" class="list-group-item list-group-item-danger">Aouter un historique de reunions</a>
+          <a href="#" class="list-group-item list-group-item-danger">Voir les historiques</a>
+          <a href="#" class="list-group-item list-group-item-danger">...</a>
+        </div>
+  </div>
+  <div class="col-md-6" >
+   <img src="../images/postit.png"  width="600" height="600" ><br/><br/> 
+  </div>
+  <div class="col-md-3" >
+    <div class="panel panel-default">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Demande d'inscription (A VALIDER) </div>
+ <!-- <div class="panel-body"></div>-->
 
+  <!-- Table -->
+  <table class="table">
+    <tr><td>nom</td><td>prenom</td><td><a href="#">Plus d'infos</a></td><td><div class="glyphicon glyphicon-ok"></div></td><td><div class="glyphicon glyphicon-remove"></div></td></tr>
+  </table>
+</div>
+
+  
+    
+  </div>
+</div>
 
 <?php
 	include("../footer.html");
