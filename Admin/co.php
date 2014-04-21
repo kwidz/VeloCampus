5b4c7309f24d19dd412aa0cb9fe6f4328198d8a9
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-04-21 20:00:13 +0200

    bug du post-it resolu

--- Admin/co.php
+++ Admin/co.php
@@ -1,3 +1,4 @@
+
 <?php
     $serveur="localhost";
     $user="velo";
--- Admin/traitement/postit.php
+++ Admin/traitement/postit.php
@@ -1,11 +1,11 @@
 <?php 
+
 require_once("../co.php");
 	if (isset($_POST['nom']) && isset($_POST['mess'])){
 		$nom = $_POST['nom'];
 		$mess=$_POST['mess'];
-
 		$sql = "INSERT INTO Postit VALUES (null,'".$nom."','".$mess."')";
-		$res = $mysqli->query($sql);
+		$res = $mysqli->query($sql) or die(mysql_error());
 	}
 	else {echo "erreur d'envoi";}
 header("Location: ".$_SERVER['HTTP_REFERER']);
