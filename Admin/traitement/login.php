4cd807dbeff2e1cefc23accf5fe03ae2c19563f7
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-21 15:46:05 +0200

    connexion

---
+++ Admin/traitement/login.php
@@ -0,0 +1,22 @@
+<?php
+	session_start();
+	if (isset($_POST['inputEmail']) && isset($_POST['inputPassword'])) {
+		$mail = $_POST['inputEmail'];
+		$pass = $_POST['inputPassword'];
+		$mysqli = new mysqli("127.0.0.1","velo","velo","velo");
+		$result = $mysqli->query("SELECT * FROM Admin WHERE pseudo_admin='".$mail."' AND password_admin='".$pass."';");
+		if($result->num_rows) {
+			$_SESSION['log']=1;
+			if (isset($_POST['remindMe']) && $_POST['remindMe'] == true) {
+				setcookie('Session', $mail, time()+2592000, "/", null);
+			}
+			else {
+				setcookie('Session', $mail, 0, "/", null);
+			}
+		}
+		else {
+			$_SESSION['log']=0;
+		}
+	}
+	header("Location: ".$_SERVER['HTTP_REFERER']);
+?>
