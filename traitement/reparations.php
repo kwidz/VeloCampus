c70cc62da8a7ec66b63da6dc8d2e209ef9b94d41
Kiwhacks <pierre.limballe@hotmail.fr>
2014-04-22 15:16:05 +0200

    Avancement du formulaire de réparation

--- traitement/reparations.php
+++ traitement/reparations.php
@@ -17,22 +17,27 @@
   function typeReparation(type) {
     switch (type) {
       case "1":
-        document.getElementById('tempsReparation').innerHTML = 
-          "<center>Le temps de réparation sera de 30 minutes.</center><br/><center>Ensuite enchainement de <select></select> pour poffiner la demande.</center>";
+        document.getElementById('descriptionReparation').innerHTML = 
+          "<center>Le temps de réparation sera de 30 minutes.</center><br/>";
         break;
 
       case "2":
-        document.getElementById('tempsReparation').innerHTML = 
-          "<center>Le temps de réparation sera d'une heure.</center><br/><center>Ensuite enchainement de <select></select> pour poffiner la demande.</center>";
+        document.getElementById('descriptionReparation').innerHTML = 
+          "<center>Le temps de réparation sera d'une heure.</center><br/>";
         break;
 
       case "3":
-        document.getElementById('tempsReparation').innerHTML = 
-          "<center>Le temps de réparation sera de 10 minutes.</center><br/><center>Ensuite enchainement de <select></select> pour poffiner la demande.</center>";
+        document.getElementById('descriptionReparation').innerHTML = 
+          "<center>Le temps de réparation sera de 10 minutes.</center><br/>";
+        break;
+
+      case "4":
+        document.getElementById('descriptionReparation').innerHTML = 
+          "<center>Ajouter une description pour préciser la réparation : <i>(50 car. max.)</i> <input type='text' class='form-control' cols='40' rows='1' placeholder='Description'/></center><br/>";
         break;
 
       default:
-        document.getElementById('tempsReparation').innerHTML = "";
+        document.getElementById('descriptionReparation').innerHTML = "";
         break;
     }
   }
@@ -48,18 +53,31 @@
               <center> Entrez le type de votre réparation : </center>
               <center> 
                 <select onchange="typeReparation(this.value)" required>
-                  <option value="0" selected></option>
+                  <option value="" selected></option>
                   <option value="1">Pneu(s) creuvé(s)</option>
                   <option value="2">Dérailleur</option>
                   <option value="3">Lampes</option>
-                </select> <br/><br/>
+                  <option value="4">Autres</option>
+                </select><br/><br/>
               </center>
             </div>
-            <div id="tempsReparation"></div>
+            <div id="descriptionReparation"></div>
+            <div>
+              <center> Urgence de votre réparation : </center>
+              <center>
+                <select required>
+                  <option value="" selected></option>
+                  <option value="1">Peu urgent</option>
+                  <option value="2">Urgent</option>
+                  <option value="3">Très urgent</option>
+                </select><br/><br/>
+              </center>
+            </div>
+            <center><input type="submit" value="Demander réparation"/></center><br/>
           </form>
         <?php }
   			else {
-		        echo "<center><br/>Vous devez vous connecter pour accéder à cette partie du site.<br/><br/></center>";
+		      echo "<center><br/>Vous devez vous connecter pour accéder à cette partie du site.<br/><br/></center>";
 		    }
 		?>
   	</div>
