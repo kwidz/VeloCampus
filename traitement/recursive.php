8e46dac3775a7248f047c5bec0b5b1ec832b4097
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-04-22 02:01:19 +0200

    ammelioration du responsive design et correction du beug de taille I LOOOOOVE CSSSSSS

--- traitement/recursive.php
+++ traitement/recursive.php
@@ -23,18 +23,18 @@ function parcourir_repertoire($repertoire)
         else
         {
             // mes modifs
-            // $tailleImage = getimagesize($repertoire.'/'.$fichier);
-            // if ($tailleImage[0] > $tailleImage[1]) {
-            //   $coeff = $tailleImage[0] / 180;
-            //   $tailleModif = $tailleImage[1]/$coeff;
-              print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="padding:15px"><img width="180px" height="130px" src="'.$repertoire.'/'.$fichier.'"/></li>';
-            // }
-            // else if ($tailleImage[1] > $tailleImage[0]) {
-            //   $coeff = $tailleImage[1] / 180;
-            //   $tailleModif = $tailleImage[0]/$coeff;
-            //   print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="padding:15px"><img width="120px" height="180px" src="'.$repertoire.'/'.$fichier.'"/></li>';
-            // }
-           // print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="padding:15px"><img height="180px" width="180px" src="'.$repertoire.'/'.$fichier.'"/></li>';
+            $tailleImage = getimagesize($repertoire.'/'.$fichier);
+            if ($tailleImage[0] > $tailleImage[1]) {
+              $coeff = $tailleImage[0] / 300;
+              $tailleModif = $tailleImage[1]/$coeff;
+              print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="margin:15px; width:190px; height:190px; overflow:hidden;"><img width="300px" height="'.$tailleModif.'px" style="clip: rect(0px, 50px, 50px, 0px);"src="'.$repertoire.'/'.$fichier.'"/></li>';
+            }
+            else if ($tailleImage[1] > $tailleImage[0]) {
+              $coeff = $tailleImage[1] / 300;
+              $tailleModif = $tailleImage[0]/$coeff;
+              print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="margin:15px; width:190px; height:190px; overflow:hidden;"><img width="'.$tailleModif.'px" height="300px" src="'.$repertoire.'/'.$fichier.'"/></li>';
+            }
+            //print '<li class="col-lg-2 col-md-2 col-sm-3 col-xs-4" style="padding:15px"><img height="180px" width="180px" style="clip: rect(0px, 50px, 50px, 0px);" src="'.$repertoire.'/'.$fichier.'"/></li>';
         }
     }
     closedir($le_repertoire);
