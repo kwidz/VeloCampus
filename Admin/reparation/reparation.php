4575d95b8740b8e5de0c10ebc2517f7caf84608b
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-24 16:14:09 +0200

    yolo

--- Admin/reparation/reparation.php
+++ Admin/reparation/reparation.php
@@ -3,5 +3,8 @@
     <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="ajouterRep.php">Ajouter des Réparations</a></li>
     <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="suppRep.php">Supprimer des Réparation</a></li>
     <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="gererRep.php">Gerer les demandes de Réparation</a></li>
+   
+
 </ul>
 </div>
+
