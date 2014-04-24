81a7ad7970b98124d2b70e269e4e40e0443d71c3
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-04-24 10:34:26 +0200

    reparationAdmin

---
+++ Admin/reparation/reparation.php
@@ -0,0 +1,7 @@
+<div class="col-md-6" >
+<ul class="nav nav-pills nav-stacked">
+    <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="ajouterRep.php">Ajouter des Réparations</a></li>
+    <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="suppRep.php">Supprimer des Réparation</a></li>
+    <li class="active" style="text-align: center; margin-bottom: 15px;"><a href="gererRep.php">Gerer les demandes de Réparation</a></li>
+</ul>
+</div>
