0097753e7a1cd6acd8bcd9d9212bd24c7de1d492
Kiwhacks <pierre.limballe@hotmail.fr>
2014-05-11 19:05:45 +0200

    Correction bug réparations client (maj bdd)

--- traitement/reparations.php
+++ traitement/reparations.php
@@ -1,108 +1,76 @@
 <?php
-	session_start();
-	include("../header.html");
-	if (isset($_SESSION['log']) && $_SESSION['log'] == 1) {
-  	include("../menulog.html");
+  session_start();
+  include("../header.html");
+  if (isset($_SESSION['log']) && $_SESSION['log'] == 1) {
+    include("../menulog.html");
     if (isset($_SESSION['rep']) && $_SESSION['rep'] == 1) {
       include("../banniereReparation.html");
       $_SESSION['rep'] = 0;
     }
-	}
-	else {
-  	include("../menu.html");
-  	if (isset($_SESSION['log']) && $_SESSION['log'] == 0) {
+  }
+  else {
+    include("../menu.html");
+    if (isset($_SESSION['log']) && $_SESSION['log'] == 0) {
       $_SESSION = array(); 
-  		include("../banniereErreurConn.html");
-  	}
-	}
+      include("../banniereErreurConn.html");
+    }
+  }
   $mysqli = new mysqli("127.0.0.1","velo","velo","velo");
 ?>
-
-<script>
-  function verifieID() {
-    if (document.getElementById("id_velo").value == -1) {
-      document.getElementById("erreurIDVelo").innerHTML='<div class="alert alert-danger">Vous n\'avez aucun vélo en location.</div>';
-      document.getElementById("type_velo").value = "0";
-    }
-    else document.getElementById("erreurIDVelo").innerHTML='';
-  }
-
-  function trouveID() {
-    if (document.getElementById("type_velo").options[document.getElementById('type_velo').selectedIndex].value == 1) {
-      document.getElementById("id_velo").value = 0;
-    }
-    else if (document.getElementById("type_velo").options[document.getElementById('type_velo').selectedIndex].value == 2) {
-      <?php 
-        $result = $mysqli->query("SELECT id_velo FROM Location l, Adherent a WHERE a.adresse_mail_adherent='".$_SESSION['mail']."' AND a.id_location=l.id_location");
-        $id = $result->fetch_array(MYSQLI_ASSOC);
-        if ($id) {
-          foreach ($id as $key => $value) {
-            $id_velo = $value;
-          }
-        }
-        else $id_velo = -1;
-      ?>
-      document.getElementById("id_velo").value = <?php echo $id_velo; ?>;
-    }
-    verifieID();
-  }
-</script>
 
 
   <?php
     if (isset($_SESSION['log']) && $_SESSION['log'] == 1) { ?>
-      <div class="row" style="background-color:#F5F5F5;border-radius:10px;border:3px solid #222222" >
-      	<div class="col-md-12">
-          <form method="post" name="reparation" action="envoyerReparations.php">
-            <br/>
-            <div>
-              <center> Votre vélo est : </center><br/>
-              <center>
-                <select class="form-control" name="type_velo" id="type_velo" onchange="trouveID()" required>
-                  <option value="" selected></option>
-                  <option value="1">Personnel</option>
-                  <option value="2">Loué à vélo campus</option>
-                </select><br/><br/>
-              </center> 
-            </div>
+      <?php
+        $result = $mysqli->query("SELECT id_velo from Location WHERE id_adherent = (SELECT id_adherent FROM Adherent WHERE adresse_mail_adherent='".$_SESSION['mail']."');");
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $id_velo = -1;
+        if ($row){
+          foreach ($row as $key => $value) {
+           $id_velo = $value;
+          }
+        }
+      if ($id_velo != -1) {
+        $result = $mysqli->query("SELECT date_location from Location WHERE id_adherent = (SELECT id_adherent FROM Adherent WHERE adresse_mail_adherent='".$_SESSION['mail']."');");
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        foreach ($row as $key => $value) {
+          $date_location = $value;
+        }
+        $tabDate = explode('-' , $date_location);
+        $date_location_fr = $tabDate[2].'/'.$tabDate[1].'/'.$tabDate[0];
+        echo "<center><b>La réparation demandée portera sur le vélo que vous louez depuis le ".$date_location_fr.".</b></center><br/>";
+        ?>
+        <div class="row" style="background-color:#F5F5F5;border-radius:10px;border:3px solid #222222" >
+          <div class="col-md-12">
+            <form method="post" name="reparation" action="envoyerReparations.php">
+              <br/>
+              <input type="hidden" name="id_velo" id="id_velo" value="<?php echo $id_velo;?>">
+              <div></div>
+              <div>
+                <center> Entrez la description de votre réparation, avec si possible la pièce à réparer : </center><br/>
+                <center><textarea class="form-control" name="description" style="height:60px" required></textarea></center>
+              </div><br/><br/>
 
-            <input type="hidden" name="id_velo" id="id_velo" value="0">
-            <center><div id="erreurIDVelo" name="erreurIDVelo"></div></center><br/>
-
-            <div></div>
-            <div>
-              <center> Entrez la description de votre réparation, avec si possible la pièce à réparer : </center><br/>
-              <center><textarea class="form-control" name="description" style="height:60px" required></textarea></center>
-            </div><br/><br/>
-
-            <div>
-              <center> Urgence de votre réparation : </center><br/>
-              <center>
-                <select class="form-control" name="urgence" required>
-                  <option value="" selected></option>
-                  <option value="1">Peu urgent</option>
-                  <option value="2">Urgent</option>
-                  <option value="3">Très urgent</option>
-                </select><br/><br/>
-              </center>
-            </div><br/>
-            <center><input type="submit" value="Demander réparation"/></center><br/>
-            <?php 
-              echo '<input type="hidden" name="mail" value="'.$_SESSION["mail"].'"/>';
-              $mysqli = new mysqli("127.0.0.1","velo","velo","velo");  
-              $result = $mysqli->query("SELECT l.id_velo FROM Location l, Adherent a WHERE a.adresse_mail_adherent='".$_SESSION["mail"]."' AND a.id_location = l.id_location");            
-              $row = $result->fetch_array(MYSQLI_ASSOC);
-              if ($row) {
-                foreach ($row as $i => $value) {
-                  echo "<input type='hidden' name='id_velo' value='".$value."'/>";
-                }
-              }
-              else echo "<input type='hidden' name='id_velo' value=''>";
-            ?>
-          </form>
+              <div>
+                <center> Urgence de votre réparation : </center><br/>
+                <center>
+                  <select class="form-control" name="urgence" required>
+                    <option value="" selected></option>
+                    <option value="1">Peu urgent</option>
+                    <option value="2">Urgent</option>
+                    <option value="3">Très urgent</option>
+                  </select><br/><br/>
+                </center>
+              </div><br/>
+              <center><input type="submit" value="Demander réparation"/></center><br/>
+            </form>
+          </div>
         </div>
-      </div>
-  <?php }
+      <?php }
+      else {
+        echo "<div class='alert alert-warning'><center>Vous devez avoir loué un vélo pour pouvoir faire une demande de réparation en ligne. Pour les vélos personnels, veuillez vous rendre directement à l'association.<br/></center></div>";
+      }
+    }
     else {
       echo "<div class='alert alert-danger'><center>Vous devez vous connecter pour accéder à cette partie du site.<br/></center></div>";
     }
@@ -110,5 +78,5 @@
 
 
 <?php
-	include("../footer.html");
+  include("../footer.html");
 ?>
