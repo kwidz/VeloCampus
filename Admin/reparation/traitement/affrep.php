43d82a3929ca4477770500c10521e948337b6786
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-05-11 17:46:16 +0200

    tble

--- Admin/reparation/traitement/affrep.php
+++ Admin/reparation/traitement/affrep.php
@@ -3,12 +3,12 @@ include('../../co.php');
 if(!empty($_GET['id_velo']) && isset($_GET['id_velo'])){
 	$sql = 'select * from Reparation where id_velo = '.$_GET['id_velo'].' and urgence=-1';
 	$res=$mysqli->query($sql);
-	echo '<table><tr><td>Reparation Effectué</td><tr>';
+	echo '<table><thead><tr><th>Reparation Effectué</th></tr></thead><tbody>';
 	while (NULL !== ($row = $res->fetch_array())) {
 
-	echo '<tr><td>'.$row[1].'</td></tr>';
+	echo '<tr><th>'.$row[1].'</th></tr>';
 }
-echo '</table>';
+echo '<tbody></table>';
 
 
 $sql = 'select * from Reparation where id_velo = '.$_GET['id_velo'].' and urgence <> -1';
