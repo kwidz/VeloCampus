d2f548848e9553caa27ea65ceedf8afb8d2ae28f
Kiwhacks <pierre.limballe@hotmail.fr>
2014-05-13 12:53:43 +0200

    Revue de la page accueil

--- accueil/index.php
+++ accueil/index.php
@@ -19,46 +19,46 @@
 <div class="row" style="background-color:#F5F5F5;border-radius:10px;border:3px solid #222222" >
   <div class="col-md-12">
   	<center>
-      <h3>
-        <?php 
-          if (isset($_COOKIE['Session']) && isset($_SESSION['log']) && $_SESSION['log'] == 1) { 
-            include("../co.php");
-            $query = "SELECT prenom_adherent FROM Adherent WHERE adresse_mail_adherent='".html_entity_decode($_COOKIE["Session"])."';";
-            $result = $mysqli->query($query);
-            $row = $result->fetch_array(MYSQLI_ASSOC);
-            if ($row) {
-              foreach ($row as $i => $value) {
-                $prenom = $value;
-                echo "Tu es de retour ".$value." !";
-              }
-
-              $query = "SELECT photo_adherent FROM Adherent WHERE prenom_adherent='".$prenom."';";
-              $result = $mysqli->query($query);
-              $row = $result->fetch_array(MYSQLI_ASSOC);
-              foreach ($row as $i => $value) {
-                if ($value) {
-                  $tailleImage = getimagesize($value);
-                  if ($tailleImage[0] > $tailleImage[1]) {
-                    $coeff = $tailleImage[0] / 100;
-                    $tailleModif = $tailleImage[1]/$coeff;
-                    echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='100' height='".$tailleModif."'/>";
-                  }
-                  else if ($tailleImage[1] > $tailleImage[0]) {
-                    $coeff = $tailleImage[1] / 100;
-                    $tailleModif = $tailleImage[0]/$coeff;
-                    echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='".$tailleModif."' height='100'/>";
-                  }
-                  else echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='100' height='100'/>";
-                }
-              }
+      <?php 
+        if (isset($_COOKIE['Session']) && isset($_SESSION['log']) && $_SESSION['log'] == 1) { 
+          include("../co.php");
+          $query = "SELECT prenom_adherent, nom_adherent FROM Adherent WHERE adresse_mail_adherent='".html_entity_decode($_COOKIE["Session"])."';";
+          $result = $mysqli->query($query);
+          $row = $result->fetch_array(MYSQLI_ASSOC);
+          if ($row) {
+            $prenom = "";
+            foreach ($row as $i => $value) {
+              $prenom = $prenom.$value." ";
             }
-            else echo $titre;
-            // else echo "Bonjour !";
-          } 
-          else echo $titre;
+            echo "Vous êtes connecté en tant que ".$prenom." !";
+
+            // $query = "SELECT photo_adherent FROM Adherent WHERE prenom_adherent='".$prenom."';";
+            // $result = $mysqli->query($query);
+            // $row = $result->fetch_array(MYSQLI_ASSOC);
+            // foreach ($row as $i => $value) {
+            //   if ($value) {
+            //     $tailleImage = getimagesize($value);
+            //     if ($tailleImage[0] > $tailleImage[1]) {
+            //       $coeff = $tailleImage[0] / 100;
+            //       $tailleModif = $tailleImage[1]/$coeff;
+            //       echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='100' height='".$tailleModif."'/>";
+            //     }
+            //     else if ($tailleImage[1] > $tailleImage[0]) {
+            //       $coeff = $tailleImage[1] / 100;
+            //       $tailleModif = $tailleImage[0]/$coeff;
+            //       echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='".$tailleModif."' height='100'/>";
+            //     }
+            //     else echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src='".$value."' width='100' height='100'/>";
+            //   }
+            // }
+            echo "<br/>"."<h3>".$titre."</h3>";
+          }
+          else echo "<h3>".$titre."</h3>";
           // else echo "Bonjour !";
-        ?>
-      </h3> 
+        } 
+        else echo "<h3>".$titre."</h3>";
+        // else echo "Bonjour !";
+      ?>
     <!-- <br/>
     Bonjour à tous et bienvenue sur notre nouveau site Internet !<br/>
     Vous trouverez ici toutes les informations sur notre association basée à Belfort et Montbéliard.<br/>
