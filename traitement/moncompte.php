30a36f6f02dff9b803ebd9e2571f005708276370
Kiwhacks <pierre.limballe@hotmail.fr>
2014-05-13 21:56:10 +0200

    Ajout bannière double mail + organisation bugs.txt

--- traitement/moncompte.php
+++ traitement/moncompte.php
@@ -6,9 +6,13 @@
   	}
     else {
       include("../menu.html");
-      if (isset($_SESSION['log']) && $_SESSION['log'] != 1) {
+      if (isset($_SESSION['log']) && $_SESSION['log'] == 0) {
           $_SESSION = array(); 
         include("../banniereErreurConn.html");
+      }
+      else if (isset($_SESSION['log']) && $_SESSION['log'] == 2) {
+         $_SESSION = array(); 
+        include("../banniereAttenteConf.html");
       }
     }
     include("../co.php");
