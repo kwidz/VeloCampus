63a8f96f8e70f10802487406564cdf16c2b0d1a3
gglangin <geoffrey.glangine@gmail.com>
2014-05-14 20:57:21 +0200

    modif pass admin

--- Admin/traitement/modifpasswd.php
+++ Admin/traitement/modifpasswd.php
@@ -5,17 +5,14 @@
   $mdp1 = md5($_POST['mdp1']);
   $mdp2 = md5($_POST['mdp2']);
   $sql="SELECT password_admin from Admin where pseudo_admin='Admin'";
-  echo $sql."<br/>";
-  echo $mdp0."<br/>";
-  echo $mdp1."<br/>";
-  echo $mdp2."<br/>";
   $query=$mysqli->query($sql);
   $mdpbase=$query->fetch_array();
   $mdpbase=$mdpbase[0];
+
   
   if ($mdp0==$mdpbase && $mdp1 == $mdp2){
     $sql="UPDATE Admin SET `password_admin` = '".$mdp1."' WHERE `Admin`.`pseudo_admin` ='Admin'";
-    
+		    
     $query=$mysqli->query($sql);
     $_SESSION['modif']="mdptrue";
   }
@@ -23,5 +20,5 @@
     $_SESSION['modif']="mdpfalse";
   }
   
- header("Location: ".$_SERVER['HTTP_REFERER']);
+  header("Location: ".$_SERVER['HTTP_REFERER']);
 ?>
