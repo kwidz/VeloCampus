75b141a476fcb8bd84c7b2f5177e2f4e9ec56a9e
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-05-14 14:32:09 +0200

    crypto mots de passes

---
+++ Admin/traitement/modifpasswd.php
@@ -0,0 +1,22 @@
+<?php
+  include("../co.php");
+  session_start();
+  $mdp0 = md5($_POST['mdp0']);
+  $mdp1 = md5($_POST['mdp1']);
+  $mdp2 = md5($_POST['mdp2']);
+  $sql="SELECT password from Admin where pseudo_admin='Admin'";
+  $query=$mysqli->query($sql);
+  $mdpbase=$query->cubrid_fetch_array($query);
+  $mdpbase=$mdpbase[0];
+
+  if ($mdp0==$mdpbase && $mdp1 == $mdp2){
+    $sql="UPDATE Admin SET `password_admin` = '".$mdp1."' WHERE `Admin`.`pseudo_admin` ='Admin'";
+    $query=$mysqli->query($sql);
+    $_SESSION['modif']="mdptrue";
+  }
+  else{
+    $_SESSION['modif']="mdpfalse";
+  }
+  
+  header("Location: ".$_SERVER['HTTP_REFERER']);
+?>
