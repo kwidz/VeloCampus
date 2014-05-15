72516ef48d0f884002c66dee254848a5c1aadda0
Kiwhacks <pierre.limballe@hotmail.fr>
2014-05-15 17:42:24 +0200

    Page vidéo client

--- traitement/videos.php
+++ traitement/videos.php
@@ -18,79 +18,12 @@ else {
 }
 ?>
 
-<style>
-ul {         
-  padding:0 0 0 0;
-  margin:0 0 0 0;
-}
-ul li {     
-  list-style:none;
-          
-}
-ul li img {
-  cursor: pointer;
-}
-.controls{          
-  width:50px;
-  display:block;
-  font-size:11px;
-  padding-top:8px;
-  font-weight:bold;          
-}
-.next {
-  float:right;
-  text-align:right;
-}
-</style>
-
-<!--   <div class="row" > -->
-    <!-- <div class="col-md-12"> -->
-
- <?php parcourir_repertoire('../images/photos/') ?>
-<!--   </div> -->
-<!--   </div> -->
-
-
-<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="dist/js/bootstrap.min.js"></script>
-
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog" id="monModalDialog">
-
-
-    <div class="modal-content" >
-
-      <div class="modal-header" style="border-style:none;">
-        <button type="button" class="close" data-dismiss="modal">x</button>
-        
-      </div>
-
-      <div class="modal-body">  
-        <script>
-        $(document).ready(function(){
-           $('li img').on('click',function(){
-                var src = $(this).attr('src');
-                var img = '<img src="' + src + '" class="img-responsive"/>';
-                $('#myModal').modal();
-                $('#myModal').on('shown.bs.modal', function(){
-                    $('#myModal .modal-body').html(img);
-                });
-                $('#myModal').on('hidden.bs.modal', function(){
-                    $('#myModal .modal-body').html('');
-                });
-           });  
-        })
-
-</script>
-
-</div>
-<div class="modal-footer" style="border-style: none;">
-    <button class="btn btn-danger" data-dismiss="modal">Fermer</button>
-</div>      
-</div><!-- /.modal-content -->
-</div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
+<center>
+  <h3> Première vidéo ! </h3>
+  <iframe width="560" height="315" src="//www.youtube.com/embed/iktNXq3K5ps" frameborder="0" allowfullscreen></iframe>
+  <!-- <iframe width="711" height="400" src="//www.youtube.com/embed/iktNXq3K5ps" frameborder="0" allowfullscreen></iframe> -->
+  <!-- 400 711 -->
+</center>
 
 <?php
 include("../footer.html");
