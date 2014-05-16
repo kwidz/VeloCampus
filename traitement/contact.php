480eb4faab05503e45236c670d54710517b3b46e
Kiwhacks <pierre.limballe@hotmail.fr>
2014-05-16 19:54:24 +0200

    Modifs page de contact

--- traitement/contact.php
+++ traitement/contact.php
@@ -32,7 +32,7 @@
  	<div class="col-md-12">
 		<br/>
 		<center>
-	  		<iframe width="1000" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=rue+engel+gros+Belfort&amp;aq=&amp;sll=47.642514,6.838968&amp;sspn=0.003271,0.008256&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Rue+Engel+Gros,+90000+Belfort&amp;ll=47.642301,6.838544&amp;spn=0.003271,0.008256&amp;z=16&amp;output=embed"></iframe>
+	  		<iframe width="1100" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=rue+engel+gros+Belfort&amp;aq=&amp;sll=47.642514,6.838968&amp;sspn=0.003271,0.008256&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Rue+Engel+Gros,+90000+Belfort&amp;ll=47.642301,6.838544&amp;spn=0.003271,0.008256&amp;z=16&amp;output=embed"></iframe>
 	  		<br />
 	  		<small>
 	  			<a href="https://maps.google.fr/maps?f=q&amp;source=embed&amp;hl=fr&amp;geocode=&amp;q=rue+engel+gros+Belfort&amp;aq=&amp;sll=47.642514,6.838968&amp;sspn=0.003271,0.008256&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=Rue+Engel+Gros,+90000+Belfort&amp;ll=47.642301,6.838544&amp;spn=0.003271,0.008256&amp;z=14" style="color:#0000FF;text-align:left">Agrandir le plan</a>
@@ -49,7 +49,7 @@
 	 	<center>
 		 	<form method="post" action="envoiMail.php">
 			 	Nom <br/><input class="form-control" type="text" name="name" id="name" required><br/><br/>
-			 	Adresse mail de contact <br/><input class="form-control" type="email" name="email" id="email" required><br/><br/>
+			 	Adresse mail pour vous contacter <br/><input class="form-control" type="email" name="email" id="email" required><br/><br/>
 			 	Sujet du message <br/><input class="form-control" type="text" name="subject" id="subject" required><br/><br/>
 			 	Votre message <br/><textarea class="form-control" style="height:300px" name="message" id="message" required></textarea><br/><br/>
 			 	<input type="submit" value="Envoyer">
--- traitement/envoiMail.php
+++ traitement/envoiMail.php
@@ -1,12 +1,12 @@
 <?php
 	session_start();
-	$EmailFrom = $_POST['email']; 
+	$Email = $_POST['email']; 
 	$EmailTo = "pierre.limballe@gmail.com";
 	$Subject = $_POST['subject'];
 	$Name = Trim(stripslashes($_POST['name'])); 
 	$Message = Trim(stripslashes($_POST['message'])); 
 	
-	$Body = "Message de : ".$Name."\n"."Contenu : \n\n".$Message;
+	$Body = "Message de : ".$Name."\n"."Adresse de contact : ".$Email."\n"."Contenu : \n\n".$Message;
 	$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 	
 	if ($success) {
