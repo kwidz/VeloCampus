5edb3fe08973e917886f6189e868aac10b129dec
Morganec <morgane.cabrol@edu.univ-fcomte.fr>
2014-05-26 14:06:47 +0200

    banniere

--- Admin/reparation/traitement/suppRep.php
+++ Admin/reparation/traitement/suppRep.php
@@ -1,17 +1,20 @@
 <?php
+session_start();
 include('../../co.php');
 if((!empty($_GET['id'])) &&(isset($_GET['id']))) {
 				$sql='delete from Reparation where id_reparation = '.$_GET['id'];
 			
 				
 				$res=$mysqli->query($sql);
+				$_SESSION['supreparation'] = 25;
 				?><script>
-					window.location='../index.php?message=ok';
+					window.location='..';
 				</script><?php
 			}
 			else{
+				$_SESSION['supreparation'] = -25;
 				?><script>
-					window.location='../index.php?message=notok';
+					window.location='..';
 				</script><?php
 			}
 ?>
--- BaseDeDonnee/script.sql
+++ BaseDeDonnee/script.sql
@@ -303,7 +303,7 @@ CREATE TABLE `Reparation` (
   PRIMARY KEY (`id_reparation`),
   KEY `id_velo` (`id_velo`),
   CONSTRAINT `fk_id_velo` FOREIGN KEY (`id_velo`) REFERENCES `Velo` (`id_velo`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -455,4 +455,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-26 13:58:04
+-- Dump completed on 2014-05-26 14:06:47
