dacf41d34c3704d0ca0d34ed493135aa3e5eb6d5
Kiwhacks <pierre.limballe@hotmail.fr>
2014-09-04 12:56:27 +0200

    Correction bug 7 mail

--- traitement/envoiMail.php
+++ traitement/envoiMail.php
@@ -1,12 +1,12 @@
 <?php
 	session_start();
 	$Email = $_POST['email']; 
-	echo "test";
 	$EmailFrom = "site@velocampus.fr";
 	$EmailTo = "pierre.limballe@gmail.com";
 	$Subject = $_POST['subject'];
 	$Name = Trim(stripslashes($_POST['name'])); 
 	$Message = Trim(stripslashes($_POST['message'])); 
+	echo "test";
 	
 	$Body = "Message de : ".$Name."\n"."Adresse de contact : ".$Email."\n"."Contenu : \n\n".$Message;
 	$success = mail($EmailTo, $Subject, $Body, "From: $EmailFrom");
