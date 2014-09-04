2ef440363b52a601cb240974861f272f86ca91eb
Kiwhacks <pierre.limballe@hotmail.fr>
2014-09-04 15:44:43 +0200

    Bug modification type de vélo

--- Admin/modèle/traitement/traitementModifier.php
+++ Admin/modèle/traitement/traitementModifier.php
@@ -5,7 +5,7 @@ if (isset($_POST["idType"]) && isset($_POST["desc"]) && isset($_POST["car"])) {
   $idType = $_POST["idType"];
   $desc = $_POST["desc"];
   $car = $_POST["car"];
-  $sql = "UPDATE _Type SET description_type = '$desc', caracteristiques_type = '$car' WHERE id_type = $idType";
+  $sql = "UPDATE _Type SET description_type = '$desc', caracteristiques_type = '$car' WHERE id_type = '$idType'";
   $res=$mysqli->query($sql);
   if ($res) $_SESSION["modMod"] = 1;
   else $_SESSION["modMod"] = -1;
