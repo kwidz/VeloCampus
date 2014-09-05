20e03916874bd803fbc82171f668ff9cdbcc8538
Kiwhacks <pierre.limballe@hotmail.fr>
2014-09-05 02:02:39 +0200

    Interface publique partenaires

--- traitement/partenaires.php
+++ traitement/partenaires.php
@@ -27,13 +27,27 @@ include("../co.php");
 $sql = "SELECT nom_partenaire, description_partenaire, photo_partenaire FROM Partenaire";
 $result = $mysqli->query($sql);
 if ($result) {
-  echo "<center>";
+  echo "<center><table width='80%'>";
+  $nom = array();
+  $description = array();
+  $photo = array();
+  $nbPar = 0;
   while (NULL !== ($row = $result->fetch_array())) {
-    echo "<h3>".$row['nom_partenaire']."</h3>";
-    echo "<i>".$row['description_partenaire']."</i><br/><br/>";
-    echo "<img heigth='200px' width='200px' src='../Admin/partenaires/".$row['photo_partenaire']."'></img><br/><br/><br/>";
+    array_push($nom, $row['nom_partenaire']);
+    array_push($description, $row['description_partenaire']);
+    array_push($photo, $row['photo_partenaire']);
+    $nbPar++;
   }
-  echo "</center>";
+  for ($i=0; $i < $nbPar; $i++) { 
+    if ($i == $nbPar-1 && $nbPar%2 == 1) {
+      echo "<tr><td><center><h3>".$nom[$i]."</h3></center></td></tr><tr><td><center><i>".$description[$i]."</i></center><br/></td></tr><tr><td><center><img heigth='200px' width='200px' src='../Admin/partenaires/".$photo[$i]."'></img></center></td></tr>";
+    }
+    else {
+      echo "<tr><td><center><h3>".$nom[$i]."</h3></center></td><td><center><h3>".$nom[$i+1]."</h3></center></td></tr><tr><td><center><i>".$description[$i]."</i></center><br/></td><td><center><i>".$description[$i+1]."</i></center><br/></td></tr><tr><td><center><img heigth='200px' width='200px' src='../Admin/partenaires/".$photo[$i]."'></img></center></td><td><center><img heigth='200px' width='200px' src='../Admin/partenaires/".$photo[$i+1]."'></img></center></td></tr>";
+      $i++;
+    }
+  }
+  echo "</table></center>";
 }
 else {
   echo "Bad :(";
